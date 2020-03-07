c7ad736aa70236ab7fc1089baecdf923ab354873
Karol Etrych <karol.etrych@gmail.com>
2020-03-07 19:49:40 +0100

    fix generate_certificate script

--- api/deployment_utils/generate_certificate.sh
+++ api/deployment_utils/generate_certificate.sh
@@ -1,2 +1,33 @@
-rm: missing operand
-Try 'rm --help' for more information.
+#!/usr/bin/env bash
+
+# Use this script for generating self-signed certificate on Arch distros 
+# cause 'dotnet dev-certs' generate invalid cert
+
+
+set -eu
+org=localhost-ca
+domain=localhost
+
+sudo trust anchor --remove ca.crt || true
+
+openssl genpkey -algorithm RSA -out ca.key
+openssl req -x509 -key ca.key -out ca.crt \
+    -subj "/CN=$org/O=$org"
+
+openssl genpkey -algorithm RSA -out "$domain".key
+openssl req -new -key "$domain".key -out "$domain".csr \
+    -subj "/CN=$domain/O=$org"
+
+openssl x509 -req -in "$domain".csr -days 365 -out "$domain".crt \
+    -CA ca.crt -CAkey ca.key -CAcreateserial \
+    -extfile <(cat <<END
+basicConstraints = CA:FALSE
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid,issuer
+subjectAltName = DNS:$domain
+END
+    )
+
+openssl pkcs12 -export -out "$domain".pfx -inkey "$domain".key -in "$domain".crt
+
+sudo trust anchor ca.crt
